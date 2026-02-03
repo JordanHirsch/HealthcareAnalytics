4f5cad0cd3b281a1652722a6b4d77da9c08905da
Jordan Hirsch <Jahirsch2001@gmail.com>
2026-02-03 17:53:16 -0500

    Create headcount by department SQL query
    Add SQL query to calculate headcount by department.

--- sql/01_headcount_by_department.sql
+++ sql/01_headcount_by_department.sql
@@ -1,1 +1,11 @@
+-- KPI: Headcount by Department
+-- Purpose: Establish staffing distribution across the facility
 
+SELECT 
+    d.department_name,
+    COUNT(e.employee_id) AS employee_count
+FROM employees e
+JOIN departments d 
+    ON e.department_id = d.department_id
+GROUP BY d.department_name
+ORDER BY employee_count DESC;
